5a247e79bf72f778dc6f5d078639d89215f2ca2e
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-02 08:48:30 -0500

    feat: add frequency pattern solution

--- 1.exercise.js
+++ 1.exercise.js
@@ -2,17 +2,43 @@
 // is an anagram of the first. An anagram is a word, phrase, or name formed
 // by rearranging the letters of another, such as cinema from iceman
 
+// Solution with arrays
+// const validAnagram = (str1, str2) => {
+//   if (str1.length !== str2.length) {
+//     return false;
+//   }
+
+//   let arr1 = [...str1].sort().join("").toString();
+//   let arr2 = [...str2].sort().join("").toString();
+//   if (arr1 === arr2) {
+//     return true;
+//   }
+//   return false;
+// };
+
+// Solution with frequency pattern
 const validAnagram = (str1, str2) => {
   if (str1.length !== str2.length) {
     return false;
   }
+  let count = {};
 
-  let arr1 = [...str1].sort().join("").toString();
-  let arr2 = [...str2].sort().join("").toString();
-  if (arr1 === arr2) {
-    return true;
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+    count[letter] ? (count[letter] += 1) : (count[letter] = 1);
   }
-  return false;
+
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+
+    if (!count[letter]) {
+      return false;
+    } else {
+      count[letter] -= 1;
+    }
+  }
+
+  return true;
 };
 
 console.log(validAnagram("anagram", "nagaram")); // true
