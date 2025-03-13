6716865b356889c5942d5f77a175a6e301982481
Yerovy Espitia <luyeesgar@gmail.com>
2025-03-12 19:54:54 -0500

    add explanation to new solution exercise 11

--- 11.find-pair.js
+++ 11.find-pair.js
@@ -16,22 +16,22 @@
 // Time Complexity Requirement - O(n log n)
 // Space Complexity Requirement - O(1)
 
-const findPair = (arr, num) => {
-  if (arr.length < 2) {
-    return false;
-  }
+// const findPair = (arr, num) => {
+//   if (arr.length < 2) {
+//     return false;
+//   }
 
-  let numSet = new Set(arr);
+//   let numSet = new Set(arr);
 
-  for (let element of arr) {
-    if (numSet.has(element + num) || numSet.has(element - num)) {
-      return true;
-    }
-  }
-  return false;
-};
+//   for (let element of arr) {
+//     if (numSet.has(element + num) || numSet.has(element - num)) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
 
-console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // true
+// console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // true
 // console.log(findPair([8, 6, 2, 4, 1, 0, 2, 5, 13], 1)); // true
 // console.log(findPair([4, -2, 3, 10], -6)); // true
 // console.log(findPair([6, 1, 4, 10, 2, 4], 22)); // false
@@ -64,18 +64,25 @@ console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // true
 // }
 
 // O(1) space + O(n log n) time
-// function findPair(arr, num) {
-//   arr.sort((a, b) => a - b);
-//   let i = 0;
-//   let j = 1;
-//   while (i < arr.length && j < arr.length) {
-//     if (i !== j && Math.abs(arr[j] - arr[i]) === Math.abs(num)) {
-//       return true;
-//     } else if (arr[j] - arr[i] < num) {
-//       j++;
-//     } else {
-//       i++;
-//     }
-//   }
-//   return false;
-// }
+function findPair(arr, num) {
+  arr.sort((a, b) => a - b);
+  let i = 0;
+  let j = 1;
+  while (i < arr.length && j < arr.length) {
+    console.log(`arr`, arr);
+
+    console.log(`pair`, [arr[j], arr[i]]);
+    console.log(`subs`, Math.abs(arr[j] - arr[i]));
+
+    if (i !== j && Math.abs(arr[j] - arr[i]) === Math.abs(num)) {
+      return true;
+    } else if (arr[j] - arr[i] < num) {
+      j++;
+    } else {
+      i++;
+    }
+  }
+  return false;
+}
+
+console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // true
