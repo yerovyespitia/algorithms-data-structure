b4abc55c72c662464885beae9e11102df298f332
Yerovy Espitia <luyeesgar@gmail.com>
2025-03-13 16:59:39 -0500

    add exercise 15

---
+++ 15.count-zeroes.js
@@ -0,0 +1,23 @@
+// Divide and Conquer - countZeroes
+// Given an array of 1s and 0s which has all 1s first followed by all 0s, write a function called countZeroes, which returns the number of zeroes in the array.
+// countZeroes([1,1,1,1,0,0]) // 2
+// countZeroes([1,0,0,0,0]) // 4
+// countZeroes([0,0,0]) // 3
+// countZeroes([1,1,1,1]) // 0
+// Time Complexity - O(log n)
+
+const countZeroes = (arr) => {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(countZeroes([1, 1, 1, 1, 0, 0])); // 2
+console.log(countZeroes([1, 0, 0, 0, 0])); // 4
+console.log(countZeroes([0, 0, 0])); // 3
+console.log(countZeroes([1, 1, 1, 1])); // 0
